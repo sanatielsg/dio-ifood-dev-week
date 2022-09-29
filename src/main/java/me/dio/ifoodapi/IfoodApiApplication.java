216819f395d2fe282db198535319bf11510331b5
Sanatiel Qualiapp <sanatielsg@gmail.com>
2022-09-29 02:48:56 -0300

    criacao do projeto

---
+++ src/main/java/me/dio/ifoodapi/IfoodApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.ifoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodApiApplication.class, args);
+	}
+
+}
